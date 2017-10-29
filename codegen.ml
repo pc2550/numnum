acb35a3707ec560b59ca6066ae40d4b40613b184
kaustubh <kaustubhgc@gmail.com>
2017-10-29 14:50:17 -0400

    strings

--- codegen.ml
+++ codegen.ml
@@ -24,12 +24,14 @@ let translate (globals, functions) =
   and i8_t   = L.i8_type   context
   and i1_t   = L.i1_type   context
   and void_t = L.void_type context
-  and float_t = L.float_type context  in
+  and float_t = L.float_type context
+  and string_t  = L.pointer_type (L.i8_type context) in
 
   let ltype_of_typ = function
       A.Int -> i32_t
     | A.Bool -> i1_t
-    | A.Void -> void_t 
+    | A.Void -> void_t
+    | A.String  -> string_t
     | A.Float -> float_t in
 
   (* Declare each global variable; remember its value in a map *)
@@ -52,14 +54,14 @@ let translate (globals, functions) =
       in let ftype = L.function_type (ltype_of_typ fdecl.A.typ) formal_types in
       StringMap.add name (L.define_function name ftype the_module, fdecl) m in
     List.fold_left function_decl StringMap.empty functions in
-  
+
   (* Fill in the body of the given function *)
   let build_function_body fdecl =
     let (the_function, _) = StringMap.find fdecl.A.fname function_decls in
     let builder = L.builder_at_end context (L.entry_block the_function) in
 
     let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
-    
+
     (* Construct the function's "locals": formal arguments and locally
        declared variables.  Allocate each on the stack, initialize their
        value, if appropriate, and remember their values in the "locals" map *)
@@ -130,7 +132,7 @@ let translate (globals, functions) =
       match L.block_terminator (L.insertion_block builder) with
 	Some _ -> ()
       | None -> ignore (f builder) in
-	
+
     (* Build the code for the given statement; return the builder for
        the statement's successor *)
     let rec stmt builder = function
